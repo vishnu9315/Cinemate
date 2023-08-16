2755052074ea6af8d11931bb3a7088c3f207303c
vishnu prasad <vspn001@gmail.com>
2023-08-16 07:48:40 +0530

    updated

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,10 +8,10 @@ export const Card = ({movie}) => {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3 translate-x-2 transform transition-transform duration-200 ease-in-out hover:scale-105">
     <Link to={`/movie/${id}`}>
-      <img className="rounded-t-lg" src={image} alt="" />
+      <img className="rounded-t-lg" src={image} alt="img" />
     </Link>
     <div className="p-5">
-      <Link to="{`/movie/${id}`}">
+      <Link to= {`/movie/${id}`}>
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
       </Link>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
